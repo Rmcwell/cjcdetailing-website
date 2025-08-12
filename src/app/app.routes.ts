2012128e5b44b7777ce9af2c1c612533bfaeeb68
Riley <rmcaldwell2015@gmail.com>
2025-08-11 22:47:51 -0400

    about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { ContactComponent } from './pages/contact/contact'
 import { ServicesComponent } from './pages/services/services';
+import { AboutComponent } from './pages/about/about';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'services', component: ServicesComponent },
+    { path: 'about', component: AboutComponent },
     { path: '**', redirectTo: '' }
 ];
